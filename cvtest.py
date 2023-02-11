71d6375aeedc76a16fa414feccd046766540675a
wangzidong <923731573@qq.com>
2023-02-11 18:12:17 +0800

    add some imgs

---
+++ cvtest.py
@@ -0,0 +1,19 @@
+import cv2 as cv
+import numpy as np
+
+print(np.array([1,1,2]))
+
+data = cv.imread('./samples/results/8.jpg')
+
+# print(data)
+
+cv.namedWindow('test',0)
+cv.resizeWindow('test',400,800)
+
+
+cv.imshow('test', data)
+
+
+
+cv.waitKey(0)
+cv.destoryAllWindows()
